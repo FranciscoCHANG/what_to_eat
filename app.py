b31e0b14a9519a3841beb83f546905b886cd1605
francisco <francisco940513@gmail.com>
2024-11-15 00:41:41 +0800

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+#-----------------------
+# 匯入模組
+#-----------------------
+from flask import Flask, render_template 
 
+#-----------------------
+# 匯入各個服務藍圖
+#-----------------------
+from services.product.app import product_bp
+
+#-------------------------
+# 產生主程式, 加入主畫面
+#-------------------------
 app = Flask(__name__)
 
+#主畫面
 @app.route('/')
 def index():
-    return "Hello, World! 這是一個簡單的餐廳搜尋網站。"
+    return render_template('index.html') 
 
+#-------------------------
+# 在主程式註冊各個服務
+#-------------------------
+app.register_blueprint(product_bp, url_prefix='/product')
+
+#-------------------------
+# 啟動主程式
+#-------------------------
 if __name__ == '__main__':
     app.run(debug=True)
